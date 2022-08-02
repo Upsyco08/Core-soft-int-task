f88dfdfbbb08a860de4435b9ac5b5582f0c55198
Sandesh Raj Shakya <sandsshakya444@gmail.com>
2022-08-02 16:16:44 +0545

    main Working on construction component

--- src/components/construction/Construction.jsx
+++ src/components/construction/Construction.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 
+import ConstructionImage from "../../assets/images/construct.jpeg" 
+
+import "./style.scss"
+
 export function Construction() {
   return (
     <div className='construction'>
         <div className='construction__left'>
-            <div className='construction__left__title'></div>
-            <div className='construction__left__description'></div>
+                <div className='construction__left__title1'>
+                    <h1 className='title-topic1'>The</h1>
+                    <h1 className='title-topic2'>contstruction</h1>
+                    <h1 className='title-topic3'>kings</h1>
+                </div>
+                <div className='construction__left__title2'>
+                    <h1 className='title-topic4'>buliding up great</h1>
+                    <h1 className='title-topic5'>things</h1>
+                </div>
+            <div className='construction__left__description'>
+                <p>Buildmax Limited is a professional building, renovation and refurbishment company. We are part of the Nairobi-based Talisman Capital Group.</p>
+            </div>
         </div>
         <div className='construction__right'>
-            <div className='construction__right__image'></div>
-            <div className='construction__right__button'></div>
+            <img className='construction__right__image' src={ConstructionImage} alt="Transnational-Bank-Plaza-Paint-work-Buildmax-2.jpg" height="807px" width="1169px"/>
+            <div className='construction__right__button'>Button</div>
         </div>
     </div>
   )
