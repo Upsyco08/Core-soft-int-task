c2e93cd9b26197bd6268e0f56dcab7a27270ad0b
Sandesh Raj Shakya <sandsshakya444@gmail.com>
2022-08-03 15:08:43 +0545

    Main Completed working on services

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,10 +2,14 @@ import interior from './interior.png';
 import windows from './windows.png';
 import rectangle from './rectangle.png';
 import construction from './construction.svg';
+import carpentry from './carpentry.svg';
+import planning from './planning.svg';
 
 export const images = {
   INTERIOR: interior,
   WINDOWS: windows,
   RECTANGLE: rectangle,
   CONSTRUCTION: construction,
+  CARPENTRY: carpentry,
+  PLANNING: planning,
 };
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -5,14 +5,14 @@ import { images } from '../../assets/images';
 import './style.scss';
 
 export function Services() {
-  const { CONSTRUCTION } = images;
+  const { CONSTRUCTION, CARPENTRY, PLANNING } = images;
   return (
     <div className='service-wrapper'>
       <h2 className='service-wrapper__title'>We Provide Below Services.</h2>
       <div className='content'>
         <div className='service'>
-          <img src={CONSTRUCTION} alt='construction' />
-          <h3>Construction</h3>
+          <img src={CARPENTRY} alt='carpentry' />
+          <h3>Retail-Fit Out & Carpentry</h3>
           <p>Lorem ipsum dolor sit amet consect etur.</p>
         </div>
         <div className='service'>
@@ -21,8 +21,8 @@ export function Services() {
           <p>Lorem ipsum dolor sit amet consect etur.</p>
         </div>
         <div className='service'>
-          <img src={CONSTRUCTION} alt='construction' />
-          <h3>Construction</h3>
+          <img src={PLANNING} alt='interior planning' />
+          <h3>interior & space planing</h3>
           <p>Lorem ipsum dolor sit amet consect etur.</p>
         </div>
       </div>
